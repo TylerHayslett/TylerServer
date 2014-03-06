10255362eb62ed07a99cf0043e3a11175d6a2c5a
Tyler Mark Hayslett <tylerthe3@gmail.com>
2014-03-06 13:57:09 -0500

    Dat First commit son

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TylerServer::Application.config.secret_key_base = '142ca6a0ae7ba0344d31b26ff1733636c6e38d3da08f3d135f2f424643698daee761f025b20c7ca3f50a4688868bfcda315650e8c1e2dc42e6379493c2064614'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TylerServer::Application.config.session_store :cookie_store, key: '_TylerServer_session'
